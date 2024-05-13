c5ca631ae92cc3c1d8a75025fe4f0e175c1a82bc
Artem Poletsky <artempoletsky@gmail.com>
2024-05-13 12:07:14 +0500

    added InitializationEffect added Events added Store Proxy object

--- store.ts
+++ store.ts
@@ -1,15 +1,30 @@
 "use client";
-import React, { Context, Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from "react";
+import React, { Context, Dispatch, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
 
-const StoreContexts: Record<string, Context<any>> = {};
-// const StoreStates: Record<string, any> = {};
-const StoreDispatchers: Record<string, Dispatch<SetStateAction<any>>> = {};
+export const StoreContexts: Record<string, Context<any>> = {};
+const StoreStates: Record<string, any> = {};
+export const StoreDispatchers: Record<string, Dispatch<SetStateAction<any>>> = {};
+const StoreListeners: Record<string, ((arg: any) => void)[]> = {};
+export const DispatcherProxies: Record<string, (arg: any) => void> = {};
 const StoreInitial: Record<string, any> = {};
-export function createStore<Type>(initial: Type) {
+
+let InitializationEffect: (() => void) | undefined = undefined;
+
+export function createStore<Type extends Record<string, any>>(initial: Type, effect?: () => void): Type {
   for (const key in initial) {
     StoreContexts[key] = createContext(initial[key]);
     StoreInitial[key] = initial[key];
   }
+  InitializationEffect = effect;
+  return new Proxy({}, {
+    get(target, key: string) {
+      return StoreStates[key];
+    },
+    set(target, key: string, value: any) {
+      DispatcherProxies[key](value);
+      return true;
+    },
+  }) as Type;
 }
 
 type ChildrenProps = {
@@ -20,23 +35,57 @@ type KeyProviderProps = ChildrenProps & {
   keys: string[];
 }
 
-export function useStoreUntyped<StoreType, KeyT extends keyof StoreType>(key: KeyT): [StoreType[KeyT], Dispatch<SetStateAction<StoreType[KeyT]>>] {
-  return [useContext(StoreContexts[key as any]), StoreDispatchers[key as any]];
+export function useStoreUntyped
+  <StoreType, KeyT extends keyof StoreType & string>
+  (key: KeyT): [StoreType[KeyT], Dispatch<SetStateAction<StoreType[KeyT]>>] {
+  return [useContext(StoreContexts[key]), DispatcherProxies[key]];
 }
 
+export function addChangeListener<StoreType, KeyT extends keyof StoreType & string>(key: KeyT, cb: (value: StoreType[KeyT]) => void) {
+  let listeners = StoreListeners[key];
+  if (!listeners) {
+    listeners = StoreListeners[key] = [];
+  }
+  listeners.push(cb);
+}
+
+export function removeChangeListener<StoreType, KeyT extends keyof StoreType & string>(key: KeyT, cb: (value: StoreType[KeyT]) => void) {
+  let listeners = StoreListeners[key];
+  if (!listeners) return;
+  StoreListeners[key] = listeners.filter(fn => fn != cb);
+}
+
+function triggerChangeListeners(key: string, value: any) {
+  let listeners = StoreListeners[key];
+  if (!listeners) return;
+  for (const cb of listeners) {
+    cb(value);
+  }
+}
 
 function KeyProvider({ children, keys }: KeyProviderProps): React.ReactNode {
   const key = keys.pop();
   if (!key) return children;
   const [value, dispatch] = useState(StoreInitial[key]);
-  // StoreStates[key] = value;
+
+  StoreStates[key] = value;
   StoreDispatchers[key] = dispatch;
+
+  const proxy = (args: any) => {
+    triggerChangeListeners(key, args);
+    dispatch(args);
+  }
+
+  DispatcherProxies[key] = proxy;
   return React.createElement(StoreContexts[key].Provider, {
     value,
   }, KeyProvider({ children, keys }));
 }
 
-export const StoreProvider: React.FC<{ children: ReactNode }> = function StoreProvider({ children }: ChildrenProps) {
+export const StoreProvider: React.FC<{ children: any }> = function StoreProvider({ children }: ChildrenProps) {
+  if (InitializationEffect) {
+    useEffect(InitializationEffect, []);
+  }
   return KeyProvider({ children, keys: Object.keys(StoreContexts) });
 }
 
